62a8f92a65dedef6f5778c8fedd8e2d7fb6e0883
moto4321 <moto432195@gmail.com>
2021-06-25 23:46:35 +0900

    210625

--- three.py
+++ three.py
@@ -68,3 +68,44 @@ def recursive_function():
 
 recursive_function()
 
+
+# 재귀 함수의 종료 조건
+  # 재귀 함수를 문제 풀이에서 사용할 떄는 재귀 함수의 종료 조건을 반드시 명시해야함
+  # 종료 조건을 제대로 명시하지 않으면 함수가 무한히 호출될 수 있다.
+    # 종료 조건을 포함한 재귀 함수 예제
+
+def recursive_function():
+    # 100번째 호출을 했을 때 종료되도록 종료 조건 명시
+    if i == 100:
+        return
+    print(i, '번째 재귀함수에서', i + 1, '번째 재귀함수를 호출합니다.')
+    recursive_function(i + 1)
+    print(i, '번째 재귀함수를 종료합니다.')
+
+recursive_function(1)
+
+
+# 팩토리얼 구현 예제
+  # n! = 1 x 2 x 3 ---- x (n - 1) x n
+  # 수학적으로 0!과 1!의 값은 1이다.
+
+# 반복적으로 구현한 n!
+def factorial_iterative(n):
+    result = 1
+    # 1부터 n까지의 수를 차례대로 곱하기
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+
+# 재귀적으로 구현한 n!
+def factorial_recursive():
+    if n <= 1: # n이 1 이하인 경우 1을 반환
+        return 1
+    # n! = n * (n - 1)!를 그대로 코드로 작성하기
+    return n * factorial_recursive(n - 1)
+
+
+# 각각의 방식으로 구현한 n! 출력(n = 5)
+print('반복적으로 구현:', factorial_iterative(5))
+print('반복적으로 구현:', factorial_recursive(5))
