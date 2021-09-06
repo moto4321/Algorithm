d5c82f14f9c93da5bd7393ad7d20e5be6d6415c6
moto4321 <moto432195@gmail.com>
2021-09-06 22:39:53 +0900

    20210906

--- test.js
+++ test.js
@@ -1,10 +1,26 @@
-const fs = require('fs')
-const inputData = fs.readFileSync('/dev/stdin').toString().split(' ').map(Number)
+(index.js)
 
-let result = 1
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
-for (let i = 2; i <= inputData[0]; i++) {
-  result += i
+
+let 초기값 = [
+  { id : 0, name : '멋진신발', quan : 2 },
+  { id : 1, name : '멋진신발2', quan : 1 }
+]
+
+function reducer(state = 초기값, 액션) {  // 여기서 등호가 들어가는 문법은 default parameter 문법(ES6 문법)
+  if (액션.type === '수량증가') {
+    
+    let copy = [...state]
+    copy[0].quan++
+
+    return copy
+  } else {
+    return state
+  }
 }
 
-console.log(result)
+let store = createStore(reducer)
+
+// reducer는 별거 아니고 그냥 수정된 state를 뱉는 함수
