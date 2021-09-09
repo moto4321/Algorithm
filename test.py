0112dfbd96d6f90a7f4f8fb2b5d0ef0cfe3f505e
moto4321 <moto432195@gmail.com>
2021-09-09 22:59:36 +0900

    20210909

--- test.py
+++ test.py
@@ -1,28 +1,3 @@
-# 3052
+arr = [3, 4, 5, 5, 5, 6, 8, 3]
 
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
-e = int(input())
-f = int(input())
-g = int(input())
-h = int(input())
-i = int(input())
-j = int(input())
-
-arr = [a, b, c, d, e, f, g, h, i, j]
-answer = []
-count = 0
-
-for m in range(len(arr)):
-    k = arr[m]%42
-    answer.append(k)
-
-for n in answer:
-    ans = n
-    if n != ans:
-        count += 1
-
-
-print(count)
+print(set(arr))
