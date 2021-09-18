91b0255e48cdfcc4b6c1f0a81b27921b212c5207
moto4321 <moto432195@gmail.com>
2021-09-18 23:40:52 +0900

    20210918

--- baekjoon2.py
+++ baekjoon2.py
@@ -1,2 +1,8 @@
 # 기본 수학 1
 # 1712
+A, B, C = map(int, input().split())
+
+if B>=C:
+    print(-1)
+else:
+    print(int(A/(C-B)+1))
--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-a, b, c = map(int, input().split())
-
-cnt = 0
-
-while True:
-    cnt += 1
-    if a + b*cnt < c*cnt:
-        print(cnt)
-        break
-    
+# 2292
