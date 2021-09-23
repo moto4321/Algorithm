3c862fec956d55690213890ebc3c4f4e10f08c66
moto4321 <moto432195@gmail.com>
2021-09-23 21:19:18 +0900

    20210923

--- baekjoon2.py
+++ baekjoon2.py
@@ -45,3 +45,35 @@ a, b, v = map(int, sys.stdin.readline().split())
 
 day = (v - b) / (a - b)
 print(math.ceil(day))
+
+# 10250
+import sys
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    h,w,n = map(int, sys.stdin.readline().split())
+
+    if n % h == 0:
+        flr = h
+        num = n // h
+    else:
+        flr = n % h
+        num = n // h + 1
+
+    print(f'{flr*100 + num}')
+
+# 2775
+t = int(input())
+
+for _ in range(t):
+    floor = int(input()) # 층
+    num = int(input()) # 호
+
+    f0 = [x for x in range(1, num + 1)] # 0층 리스트
+    
+    for k in range(floor): # 층 수 만큼 반복
+        for i in range(1, num): # 1 ~ n-1까지 (인덱스로 사용)
+            f0[i] += f0[i-1] # 층별 각 호실의 사람 수를 변경
+    print(f0[-1]) # 가장 마지막 수 출력
+
--- test.py
+++ test.py
@@ -1,7 +1,12 @@
-import sys
-import math
+t = int(input())
 
-a, b, v = map(int, sys.stdin.readline().split())
+for _ in range(t):
+    floor = int(input()) # 층
+    num = int(input()) # 호
 
-day = (v - b) / (a - b)
-print(math.ceil(day))
+    f0 = [x for x in range(1, num + 1)] # 0층 리스트
+    
+    for k in range(floor): # 층 수 만큼 반복
+        for i in range(1, num): # 1 ~ n-1까지 (인덱스로 사용)
+            f0[i] += f0[i-1] # 층별 각 호실의 사람 수를 변경
+    print(f0[-1]) # 가장 마지막 수 출력
