e540611b60316ccc8807299a03cfd59da159316f
moto4321 <moto432195@gmail.com>
2021-09-24 23:31:10 +0900

    20210924

--- baekjoon2.py
+++ baekjoon2.py
@@ -77,3 +77,18 @@ for _ in range(t):
             f0[i] += f0[i-1] # 층별 각 호실의 사람 수를 변경
     print(f0[-1]) # 가장 마지막 수 출력
 
+
+# 2839
+n = int(input()) # 설탕
+
+result = 0 # 봉지 수
+
+while n >= 0:
+    if n % 5 == 0: # 5로 나눈 나머지가 0인 경우
+        result += n // 5 # 5로 나눈 몫 추력
+        print(result)
+        break
+    n -= 3 # 설탕이 5의 배수가 될때까지 반복
+    result += 1 # 봉지 추가
+else:
+    print(-1) # while문이 거짓이 되면 -1 출력
--- test.py
+++ test.py
@@ -1,12 +1,15 @@
-t = int(input())
+n = int(input())
 
-for _ in range(t):
-    floor = int(input()) # 층
-    num = int(input()) # 호
-
-    f0 = [x for x in range(1, num + 1)] # 0층 리스트
-    
-    for k in range(floor): # 층 수 만큼 반복
-        for i in range(1, num): # 1 ~ n-1까지 (인덱스로 사용)
-            f0[i] += f0[i-1] # 층별 각 호실의 사람 수를 변경
-    print(f0[-1]) # 가장 마지막 수 출력
+cnt = n // 5
+rst = n % 5
+if rst == 0:
+    print(cnt)
+else:
+    # rst 1 ~ 4 
+    if rst == 3:
+        print(cnt + 1)
+    else:
+        if n % 3 == 0:
+            print(n // 3)
+        else:
+            print(-1)
