c18e1368cf0902df5a61dde310f3d3cbe5238e83
moto4321 <moto432195@gmail.com>
2021-10-04 18:51:22 +0900

    20211004

--- baekjoon2.py
+++ baekjoon2.py
@@ -120,3 +120,26 @@ for i in li:
             answer += 1
         
 print(answer)
+
+
+# 2581
+m = int(input())
+n = int(input())
+
+li = []
+
+for i in range(m, n + 1):
+    error = 0
+    if i > 1:
+        for j in range(2, i):
+            if i % j == 0:
+                error += 1
+                break
+        if error == 0:
+            li.append(i)
+    
+if len(li) > 0:
+    print(sum(li))
+    print(min(li))
+else:
+    print(-1)
