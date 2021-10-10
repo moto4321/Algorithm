c3e70dffbbd72812139c68beba86de5906ffc36f
moto4321 <moto432195@gmail.com>
2021-10-10 16:50:07 +0900

    20211010

--- baekjoon2.py
+++ baekjoon2.py
@@ -188,7 +188,7 @@ import math
 
 result=[]
 
-def c(a,b):
+def sosu(a,b):
     cnt=0
     check = [True] * (b+1) 
     
@@ -206,7 +206,29 @@ while True:
     a=int(input())
     if a==0:
         break
-    result.append(c(a,2*a))
+    result.append(sosu(a,2*a))
     
 for i in result:
     print(i)
+
+
+# 4948  # 또 다른 풀이 # 시간초과...
+def sosu(x):
+    if x == 1:            #1은 모든 수의 약수이기 때문에 pass
+        return False
+    for i in range(2,int(x**0.5)+1):  #제곱근이 있는 수 중에
+        if x % i == 0:					#약수가 있으면 false	
+            return False
+    return True						#이외에는 소수
+
+
+
+while True:
+    n = int(input())				#범위를 주기 위한 입력
+    cnt = 0
+    if n == 0 :						#0 입력하면 아웃되는 게 조건
+        break
+    for i in range(n,2*n + 1):		#n과 2n+1사이에서
+        if sosu(i):					#sosu함수안에 있는 게 있다면
+            cnt += 1					#카운트를 세라
+    print(cnt)		#개수를 출력하는 조건에 맞춰 카운트를 출력
