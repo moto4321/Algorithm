592bcdb716df83b8ed085d280c2262f6f3ab0f68
moto4321 <moto432195@gmail.com>
2021-10-18 21:28:59 +0900

    20211018

--- four.py
+++ four.py
@@ -1,15 +1,61 @@
 # 선택정렬
-li = [1, 5, 3, 7, 4, 9]
+arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
-for i in range(len(li)):
+for i in range(len(arr)):
     min_index = i
-    for j in range(i + 1, len(li)):
-        if li[min_index] > li[j]:
+    for j in range(i + 1, len(arr)):
+        if arr[min_index] > arr[j]:
             min_index = j
-    li[min_index], li[i] = li[i], li[min_index]
+    arr[min_index], arr[i] = arr[i], arr[min_index]
 
-print(li)
+print(arr)
 
 
 
 # 삽입정렬
+arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(1, len(arr)):
+    for j in range(i, 0, -1):
+        if arr[j] < arr[j - 1]:
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]
+        else:
+            break
+
+print(arr)
+
+
+# 퀵 정렬
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array):
+    # 리스트가 하나 이하의 원소만을 담고 있다면 종료
+    if len(array) <= 1:
+        return array
+    pivot = array[0] # 피벗은 첫 번째 원소
+    tail = array[1:] # 피벗을 제외한 리스트
+    
+    left_side = [x for x in tail if x <= pivot] # 분할된 왼쪽 부분
+    right_side = [x for x in tail if x > pivot] # 분할된 오른쪽 부분
+
+    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행하고, 전체 리스트 반환
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+print(quick_sort(array))
+
+
+
+# 계수정렬
+# 모든 원소의 값이 0보다 크거나 같다고 가정
+array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
+# 모든 범위를 포함하는 리스트 선언(모든 값은 0으로 초기화)
+count = [0] * (max(array) + 1)
+
+for i in range(len(array)):
+    count[array[i]] += 1 # 각 데이터에 해당하는 인덱스 값 증가
+
+for i in range(len(count)): # 리스트에 기록된 정렬 정보 확인
+    for j in range(count[i]):
+        print(i, end=' ') # 띄어쓰기를 구분으로 등장한 횟수만큼 인덱스 출력
+
+
