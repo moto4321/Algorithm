644445d3fd39fee939ad8e3615ca97491291eb30
moto4321 <moto432195@gmail.com>
2021-11-03 22:24:56 +0900

    20211103

--- seven.py
+++ seven.py
@@ -73,6 +73,48 @@ for i in range(1, n + 1):
 
 
 
+# 우선순위 큐(Priority Queue)
+# 최소 힙
+import heapq
+
+# 오름차순 힙 정렬
+def heapsort(iterable):
+    h = []
+    result = []
+    # 모든 원소를 차례대로 힙에 삽입
+    for value in iterable:
+        heapq.heappush(h, value)
+    # 힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
+    for i in range(len(h)):
+        result.append(heapq.heappop(h))
+    return result
+
+result = heapsort([1,3,5,7,9,2,4,6,8,0])
+print(result)
+
+# 최대 힙
+import heapq
+
+# 내림차순 힙 정렬(Heap Sort)
+def heapsort(iterable):
+    h = []
+    result = []
+    # 모든 원소를 차례대로 힙에 삽입
+    for value in iterable:
+        heapq.heappush(h, -value) # 부호를 바꾸어 넣는다
+    # 힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
+    for i in range(len(h)):
+        result.append(-heapq.heappop(h)) # 꺼낼 때 부호를 바꾸어 꺼낸다
+    return result
+
+result = heapsort([1,3,5,7,9,2,4,6,8,0])
+print(result)
+
+# heapq를 이용한 다익스트라 알고리즘
+
+# 플로이드 워셜 알고리즘
+
+
 
 # 테스트 케이스(Test Case) 입력
 for tc in range(int(input())):
