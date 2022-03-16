fcf9c0f7e2bb05ec99cb0bc3bfb286203ed74077
moto4321 <moto432195@gmail.com>
2022-03-17 00:19:46 +0900

    a few edit

--- hanghae/algorithm_week1/day5/303.py
+++ hanghae/algorithm_week1/day5/303.py
@@ -1,16 +1,21 @@
 # 중복 문자 없는 가장 긴 부분 문자열
 # leetcode 3
 
-used = {}
-max_length = start = 0
-for index , char in enumerate(s):
-    # 이미 등장했던 ㅁ누자라면 'start' 위치 갱신
-    if char in used and start <= used[char]:
-        start = used[char] + 1
-    else: # 최대 부분 문자열 길이 갱신
-        max_length = max(max_length, index - start + 1)
+s = 'abcabcbb'
 
-    # 현재 문자의 위치 삽입
-    used[char] = index
+def lengthOfLongestSubstring(s):
+    used = {}
+    max_length = start = 0
+    for index , char in enumerate(s):
+        # 이미 등장했던 문자라면 'start' 위치 갱신
+        if char in used and start <= used[char]:
+            start = used[char] + 1
+        else: # 최대 부분 문자열 길이 갱신
+            max_length = max(max_length, index - start + 1)
 
-return max_length
+        # 현재 문자의 위치 삽입
+        used[char] = index
+
+    return max_length
+
+print(lengthOfLongestSubstring(s))
--- hanghae/algorithm_week1/day5/307.py
+++ hanghae/algorithm_week1/day5/307.py
@@ -1,21 +1,27 @@
 # 상위 K 빈도 요소
 # leetcode 347
-import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        import heapq
 
-freqs = collections.Counter(nums)
-freqs_heap = []
-# Counter({1: 3, 2: 2, 3: 1})
+        freqs = collections.Counter(nums)
+        freqs_heap = []
+        # Counter({1: 3, 2: 2, 3: 1})
 
-for f in freqs:
-    heapq.heappush(freqs_heap, (-freqs[f], f))
+        for f in freqs:
+            heapq.heappush(freqs_heap, (-freqs[f], f))
 
-topk = list()
+        print(freqs_heap)
 
-for _ in range(k):
-    topk.append(heapq.heappop(freqs_heap)[1])
+        topk = list()
 
-return topk
+        for _ in range(k):
+            topk.append(heapq.heappop(freqs_heap)[1])
+
+        return topk
 
 
 # 파이썬 다운 풀이
-return list(zip(*collections.Counter(nums).most_common(k)))[0]
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        return list(zip(*collections.Counter(nums).most_common(k)))[0]
