c5a7124a9d6280629e89d7d9cd56f6f74fc0bcf6
moto4321 <moto432195@gmail.com>
2022-03-28 13:34:55 +0900

    quick sort edit

--- hanghae/algorithm_week3/day15_quicksort/508.py
+++ hanghae/algorithm_week3/day15_quicksort/508.py
@@ -1,2 +1,24 @@
 # 색 정렬
-# leetcode 
+# leetcode
+
+def quicksort(lst, start, end):
+    def partition(part, ps, pe):
+        pivot = part[pe]
+        i = ps - 1
+        for j in range(ps, pe):
+            if part[j] < pivot:
+                i += 1
+                part[i], part[j] = part[j], part[i]
+
+        part[i + 1], part[pe] = part[pe], part[i + 1]
+        return i + 1
+
+    if start >= end:
+        return None
+
+    p = partition(lst, start, end)
+    quicksort(lst, start, p - 1)
+    quicksort(lst, p + 1, end)
+    return lst
+
+quicksort(nums, 0, len(nums) - 1)
