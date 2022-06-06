c152d36df8dc8cb4b5d136fff6a731c70be66205
moto4321 <moto432195@gmail.com>
2022-06-06 23:51:24 +0900

    boj 1541

--- hanghae/algorithm_study/week3/boj1541.py
+++ hanghae/algorithm_study/week3/boj1541.py
@@ -1,1 +1,17 @@
 # 잃어버린 괄호
+n = input()
+m = n.split('-')
+
+answer = 0
+# 첫번째는 -로 시작할 경우의 수가 있어서 따로 작업
+x = sum(map(int, (m[0].split('+'))))
+if n[0] == '-':
+    answer -= x
+else:
+    answer += x
+
+for x in m[1:]: # 첫번째 작업은 이미 했기 때문에 인덱스 1부터 시작
+    x = sum(map(int, (x.split('+'))))
+    answer -= x
+
+print(answer)
