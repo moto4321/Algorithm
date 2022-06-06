2000d87925dcbd96033a41ed72a4bbbc2f3aeaa3
moto4321 <moto432195@gmail.com>
2022-06-06 22:30:31 +0900

    boj 11050

---
+++ hanghae/algorithm_study/week3/boj11050.py
@@ -0,0 +1,10 @@
+# 이항 계수1
+n, k = map(int, input().split())
+
+def factorial(x):
+    ans = 1
+    for i in range(2, x + 1):
+        ans *= i
+    return ans
+
+print(factorial(n) / factorial(k) / factorial(n - k))
