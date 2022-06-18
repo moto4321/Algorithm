6227d9b32d4d691004ae88f96c3baa854fd87439
moto4321 <moto432195@gmail.com>
2022-06-18 22:39:51 +0900

    boj 9461

---
+++ hanghae/algorithm_study/week4/boj9461.py
@@ -0,0 +1,11 @@
+# 파도반 수열
+wh = [0 for i in range(101)]
+wh[1] = 1
+wh[2] = 1
+wh[3] = 1
+for i in range(0, 98):
+    wh[i + 3] = wh[i] + wh[i + 1]
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    print(wh[n])
