19de32b657294000cb02b28a4aee6cbd93751159
moto4321 <moto432195@gmail.com>
2022-07-18 20:21:56 +0900

    boj 1202

---
+++ boj/greedy/boj1202.py
@@ -0,0 +1,28 @@
+# 보석 도둑
+import heapq
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+# 2, 1
+
+jewel_list = [list(map(int, input().split())) for _ in range(n)]
+# [[5, 10], [100, 100]]
+bag_list = [int(input()) for _ in range(k)]
+# 11
+jewel_list.sort()
+bag_list.sort()
+
+result = 0
+temp = []
+
+for bag_weight in bag_list:
+    while jewel_list and bag_weight >= jewel_list[0][0]:
+        heapq.heappush(temp, -jewel_list[0][1]) # Max heap
+        heapq.heappop(jewel_list)
+    if temp:
+        result += heapq.heappop(temp)
+    elif not jewel_list:
+        break
+
+print(-result)
