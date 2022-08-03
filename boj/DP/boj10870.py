08effd277ce635a75926cd1227fb4ea98dc153f6
moto4321 <moto432195@gmail.com>
2022-08-03 18:08:33 +0900

    boj 10870

---
+++ boj/DP/boj10870.py
@@ -0,0 +1,11 @@
+# 피보나치 수 5
+n = int(input())
+d = [0] * (n + 1)
+
+if n != 0:
+    d[1] = 1
+    for i in range(2, n + 1):
+        if d[i] == 0:
+            d[i] = d[i - 1] + d[i - 2]
+
+print(d[-1])
